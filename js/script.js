70e6d6a20ba843d701d54e6440e6ab8dc0318be5
Sardor-Develoer <sardor.sanginov09@icloud.com>
2025-01-29 15:55:02 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const  hamburger = 5;
+// const  fries = 0;
+
+
+// if (hamburger   && fries){
+//     console.log('Я сыт!');
+    
+// }
+
+
+
+
+const  hamburger = 2;
+const  fries = 1;
+
+
+if (hamburger === 3 && fries){
+    console.log('Я сыт!');
+    
+}
